a68204aa35533663f8000ec2d7dc1f4f2de79144
yangyuanliang <yangyuanliang@flashhold.com>
2019-01-24 20:05:17 +0800

    webservice

--- src/main/java/com/yyl/one/collection/ConcurrentListQueueTest.java
+++ src/main/java/com/yyl/one/collection/ConcurrentListQueueTest.java
@@ -1,11 +1,15 @@
 package com.yyl.one.collection;
 
+import java.util.ArrayList;
 import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 /**
  * @author yyl
  * @date 2018/11/18 下午6:53
  */
-public class ConcurrentListQueueTest {
+public class ConcurrentListQueueTest extends  AbstractQueuedSynchronizer{
     //ConcurrentLinkedDeque
+    //ArrayList
+
 }
