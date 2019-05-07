72368305e8ffc415d99ac67ea10a7fa1bed8e06e
System Administrator <root@liangliangdeMacBook-Pro.local>
2019-05-07 21:06:35 +0800

    study

---
+++ src/main/java/com/yyl/one/offer/Solution7.java
@@ -0,0 +1,26 @@
+package com.yyl.one.offer;
+
+/**
+ * 题目描述
+ * 大家都知道斐波那契数列，现在要求输入一个整数n，
+ * 请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+ * n<=39
+ */
+public class Solution7 {
+    public int Fibonacci(int n) {
+        int a=1,b=1,c=0;
+        if(n<0){
+            return 0;
+        }else if(n==1||n==2){
+            return 1;
+        }else{
+            for(int i=3;i<=n;i++){
+                c=a+b;
+                b=a;
+                a=c;
+            }
+            return c;
+        }
+
+    }
+}
