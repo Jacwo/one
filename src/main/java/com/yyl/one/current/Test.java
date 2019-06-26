1bf2fdd62526748b47263e81dbd53c3f74077af6
杨园亮 <yangyuanliang@yangyuanliangdeMacBook-Pro.local>
2019-06-26 15:39:22 +0800

    提交

---
+++ src/main/java/com/yyl/one/current/Test.java
@@ -0,0 +1,19 @@
+package com.yyl.one.current;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Test {
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger=new AtomicInteger();
+        atomicInteger.incrementAndGet();
+
+        atomicInteger.set(2222);
+        boolean b = atomicInteger.compareAndSet(2222, 2);
+        System.out.println(b);
+
+        int i = atomicInteger.get();
+        System.out.println(i);
+
+    }
+}
