c7d05f347396c7b69de8d1dea35ba9d312bb04db
yangyuanliang <759519274@qq.com>
2019-09-03 17:21:45 +0800

    最长回文子串

---
+++ src/main/java/com/yyl/one/leetcode/LongestPalindrome.java
@@ -0,0 +1,36 @@
+package com.yyl.one.leetcode;
+
+/**
+ * author:yangyuanliang Date:2019-09-03 Time:17:17
+ **/
+public class LongestPalindrome {
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public static int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
+
+    public static void main(String[] args) {
+        String s="abccba";
+        System.out.println(longestPalindrome(s));
+    }
+}
