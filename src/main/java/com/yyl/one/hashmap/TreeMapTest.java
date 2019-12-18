3725f303b58d4935d5baa343070193f07f1c39f2
yangyuanliang <759519274@qq.com>
2019-12-18 11:53:38 +0800

    更新

---
+++ src/main/java/com/yyl/one/hashmap/TreeMapTest.java
@@ -0,0 +1,22 @@
+package com.yyl.one.hashmap;
+
+import java.util.*;
+
+/**
+ * author:yangyuanliang Date:2019-12-18 Time:10:13
+ **/
+public class TreeMapTest {
+    public static void main(String[] args) {
+        Map<String,String> map=new Hashtable<>(5);
+        map.put("5","5");
+        map.put("1","1");
+        map.put("2","2");
+        map.put("3","3");
+        map.put("4","4");
+        map.put(null,null);
+        for(Map.Entry entry:map.entrySet()){
+            System.out.println(entry.getValue());
+        }
+
+    }
+}
