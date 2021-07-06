c85eea7672180bf8a89b44fea681d6cdb90f59f1
yangyuanliang <759519274@qq.com>
2021-07-06 18:02:56 +0800

    update

--- src/main/java/com/yyl/one/array/InsertSortTest.java
+++ src/main/java/com/yyl/one/array/InsertSortTest.java
@@ -8,7 +8,7 @@ package com.yyl.one.array;
 public class InsertSortTest {
 
     public static void insertSort(int arr[] ){
-        for(int i=0;i<arr.length;i++){
+        for(int i=1;i<arr.length;i++){
             for(int j=i;j>0;j--){
                 if(arr[j]<arr[j-1]){
                     int temp=arr[j];
--- src/main/java/com/yyl/one/array/MergeSortTest.java
+++ src/main/java/com/yyl/one/array/MergeSortTest.java
@@ -6,43 +6,42 @@ package com.yyl.one.array;
  * http://118.31.9.84:3001/articleDetail?article_id=39
  **/
 public class MergeSortTest {
-    public static void sort(int arr[],int left,int right){
-        if(left == right) {
-            return;
-        }
-        int mid=(left+right)/2;
-        sort(arr,left,mid);
-        sort(arr,mid+1,right);
-        merge(arr,left,mid,right);
-    }
+	public static void sort(int arr[], int left, int right) {
+		if (left == right) {
+			return;
+		}
+		int mid = (left + right) / 2;
+		sort(arr, left, mid);
+		sort(arr, mid + 1, right);
+		merge(arr, left, mid, right);
+	}
 
-    private static void merge(int[] arr, int left,int mid, int right) {
-        int temp[]=new int[right-left+1];
-        int p1=left;
-        int p2=mid+1;
-        int i=0;
-        while (p1<=mid && p2<=right){
-            temp[i++]=arr[p1]<arr[p2]?arr[p1++]:arr[p2++];
-        }
-        while (p1<=mid){
-            temp[i++]=arr[p1++];
-        }
-        while (p2<=right){
-            temp[i++]=arr[p2++];
-        }
-        for(int j=0;j<temp.length;j++){
-            arr[left+j]=temp[j];
-        }
+	private static void merge(int[] arr, int left, int mid, int right) {
+		int temp[] = new int[right - left + 1];
+		int p1 = left;
+		int p2 = mid + 1;
+		int i = 0;
+		while (p1 <= mid && p2 <= right) {
+			temp[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+		}
+		while (p1 <= mid) {
+			temp[i++] = arr[p1++];
+		}
+		while (p2 <= right) {
+			temp[i++] = arr[p2++];
+		}
+		for (int j = 0; j < temp.length; j++) {
+			arr[left + j] = temp[j];
+		}
 
 
+	}
 
-    }
-
-    public static void main(String[] args) {
-        int a[]={2,3,43,5};
-        sort(a,0,a.length-1);
-        for (int i:a){
-            System.out.println(i);
-        }
-    }
+	public static void main(String[] args) {
+		int a[] = {2, 3, 43, 5};
+		sort(a, 0, a.length - 1);
+		for (int i : a) {
+			System.out.println(i);
+		}
+	}
 }
